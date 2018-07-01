fb3022a752a50c660ab9c7ad62621f8a09e7b9bf
sejalgupta12 <39177562+sejalgupta12@users.noreply.github.com>
2018-07-01 19:01:35 +0530

    first commit

---
+++ expressapp/model/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt');
+
+  var schema = new Schema({
+  
+   username:{type:String,require:true},
+   email:{type:String,require:true},
+   password:{type:String,require:true},
+   creation_dt:{type:Date,require:true}
+  });
+
+  schema.statics.hashPassword=function hashPassword(password)
+  {
+      return bcrypt.hashSync(password,10);
+  }
+
+  schema.methods.isValid = function(hashedPassword)
+  {
+      return bcrypt.compareSync(hashedPassword,this.password);
+  }
+
+    module.exports=mongoose.model('User',schema);
